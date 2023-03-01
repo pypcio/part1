e1cad018eeeedd7b08deaf9d3f2b5a9a56c0ab90
Szymon Tymcio <s.tymcio98@gmail.com>
2023-03-01 16:13:31 +0100

    courseinfo ex1.2

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -4,23 +4,26 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 const Content = ({
-  exercises1,
-  exercises2,
-  exercises3,
   part1,
   part2,
   part3,
+  exercises1,
+  exercises2,
+  exercises3,
 }) => {
   return (
     <React.Fragment>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </React.Fragment>
+  );
+};
+const Part = ({ exercises, part }) => {
+  return (
+    <React.Fragment>
       <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
+        {part} {exercises}
       </p>
     </React.Fragment>
   );
