fa690d32d142f1b43fc0ffc2db003efad0b39471
Szymon Tymcio <s.tymcio98@gmail.com>
2023-03-01 17:59:52 +0100

    courseinfo ex1.4

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -3,23 +3,16 @@ import React from "react";
 const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
-const Content = ({
-  part1,
-  part2,
-  part3,
-  exercises1,
-  exercises2,
-  exercises3,
-}) => {
+const Content = ({ parts }) => {
   return (
     <React.Fragment>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
     </React.Fragment>
   );
 };
-const Part = ({ exercises, part }) => {
+const Part = ({ part, exercises }) => {
   return (
     <React.Fragment>
       <p>
@@ -37,26 +30,22 @@ const Total = ({ exercises1, exercises2, exercises3 }) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = { name: "Fundamentals of React", exercises1: 10 };
-  const part2 = { name: "Using props to pass data", exercises2: 7 };
-  const part3 = { name: "State of a component", exercises3: 14 };
-
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
   return (
     <React.Fragment>
-      <Header course={course} />
-      <Content
-        part1={part1.name}
-        part2={part2.name}
-        part3={part3.name}
-        exercises1={part1.exercises1}
-        exercises2={part2.exercises2}
-        exercises3={part3.exercises3}
-      />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Total
-        exercises1={part1.exercises1}
-        exercises2={part2.exercises2}
-        exercises3={part3.exercises3}
+        exercises1={course.parts[0].exercises}
+        exercises2={course.parts[1].exercises}
+        exercises3={course.parts[2].exercises}
       />
     </React.Fragment>
   );
