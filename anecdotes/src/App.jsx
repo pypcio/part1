5f5550f442c4476a024871e92ad9203e4c70b6f8
Szymon Tymcio <s.tymcio98@gmail.com>
2023-03-06 11:44:34 +0100

    anecdotes ex1.12

---
+++ anecdotes/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const Button = ({ handle }) => {
+  return <button onClick={handle}>random</button>;
+};
+const App = () => {
+  const [selected, setSelected] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const generateRandomNumber = (maxRange) => {
+    return Math.floor(Math.random() * maxRange);
+  };
+  const handleSelected = () => {
+    const temp = generateRandomNumber(anecdotes.length);
+    setSelected(temp);
+  };
+
+  return (
+    <div>
+      <h4>{anecdotes[selected]}</h4>
+      <Button handle={handleSelected} />
+    </div>
+  );
+};
+
+export default App;
